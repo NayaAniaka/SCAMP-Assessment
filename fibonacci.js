2c8baad57d8577f9ff6e1b92e4b42d097ad4c004
NayaAniaka <64979125+NayaAniaka@users.noreply.github.com>
2020-05-07 18:37:41 +0100

    Update fibonacci.js
    Added comments and expanded argument names to be more descriptive

--- fibonacci.js
+++ fibonacci.js
@@ -1,4 +1,8 @@
-function fibonacci(limit, curr, prev = 0, res = []) { 
-    res.push(prev);
-    return limit <= 0 ? res : fibonacci(limit - 1, curr + prev, curr, res);
+// limit - sets the bounds on range of sequence
+// current - the number to compute the sequence for
+// previous (optional) - the number preceding curr
+// result (optional) - the array that will hold the results of the computation, returned at the end 
+function fibonacci(limit, current, previous = 0, result = []) { 
+    result.push(previous);
+    return limit <= 0 ? result : fibonacci(limit - 1, current + previous, current, result);
 }
